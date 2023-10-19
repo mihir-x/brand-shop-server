9163bb42c5e66ece1bb2ed1a1e8c4c72f8a643de
mihir-x <ictlancerx@gmail.com>
2023-10-19 14:17:27 +0600

    added api to get specific product

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -58,6 +58,15 @@ async function run() {
         app.post('/products', async(req, res) =>{
             const products = req.body
             const result = await productCollection.insertOne(products)
+            res.send(result)
+        })
+        //get specific products from database
+        app.get('/products/:id', async(req, res) =>{
+            const id = req.params.id
+            const query = {
+                _id: new ObjectId(id),
+            }
+            const result = await productCollection.findOne(query)
             res.send(result)
         })
 
